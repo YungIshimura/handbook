c11d2b379a3cb6b548b7097b1cdd4ce213834b77
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-03-03 09:45:03 +0200

    Шаблонизировал about.html

--- geo_handbook/urls.py
+++ geo_handbook/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
 from django.urls import path
-from .views import view_index
+from .views import view_index, view_about
 
 
 app_name = 'geo_handbook'
 
 urlpatterns = [
-    path('', view_index, name='index')
+    path('', view_index, name='index'),
+    path('about/', view_about, name='about')
 ]
