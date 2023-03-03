d9c34be5219059a63fa1043398be06dad23cc723
axexeyvershinin <og2001@yandex.ru>
2023-03-03 12:30:30 +0300

    Templated sign_up_user.html

--- geo_handbook/urls.py
+++ geo_handbook/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import view_index
-
+from .views import view_index, view_sign_up_user
 
 app_name = 'geo_handbook'
 
 urlpatterns = [
-    path('', view_index, name='index')
+    path('', view_index, name='index'),
+    path('sign_up_user/', view_sign_up_user, name='sign_up_user')
 ]
--- geo_handbook/views.py
+++ geo_handbook/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def view_index(request):
-    return render (request, 'index.html')
+    return render(request, 'index.html')
+
+
+def view_sign_up_user(request):
+    return render(request, 'sign_up_user.html')
