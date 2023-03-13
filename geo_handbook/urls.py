bca08a0b0f87a341336294084c98204a09aee04c
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-03-13 12:03:08 +0200

    Comment urls

--- geo_handbook/urls.py
+++ geo_handbook/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (view_about, view_select, view_selected_region, view_application,
                     view_enter_details_company, view_settings_profile, view_index, view_sign_up_user,
-                    view_sign_up_company, view_select_city, view_search, view_card_layer, company_update,
-                    update_branch, delete_branch, add_branch, delete_employee_company, add_employee_company,
+                    view_sign_up_company, view_select_city, view_search, view_card_layer, #company_update
+                    delete_branch, delete_employee_company, add_employee_company,
                     update_employee_company, delete_employee_branch, update_employee_branch, add_employee_branch,
-                    view_card, view_profile, view_rates)
+                    view_card, view_profile, view_rates,
+                    #  update_branch, add_branch,
+)       
 
 app_name = 'geo_handbook'
 
@@ -25,10 +27,10 @@ urlpatterns = [
     path('profile/', view_profile, name='profile'),
     path('rates/', view_rates, name='rates'),
 
-    path('company/edit/<int:pk>/', company_update, name='edit_company'),
-    path('branches/edit/<int:pk>/', update_branch, name='edit_branches'),
+    # path('company/edit/<int:pk>/', company_update, name='edit_company'),
+    # path('branches/edit/<int:pk>/', update_branch, name='edit_branches'),
     path('branches/delete/<int:pk>/', delete_branch, name='delete_branches'),
-    path('company/edit/<int:pk>/add_branch/', add_branch, name='add_branches'),
+    # path('company/edit/<int:pk>/add_branch/', add_branch, name='add_branches'),
 
     path('company/employee/delete/<int:pk>/', delete_employee_company, name='delete_employee_company'),
     path('company/employee/edit/<int:pk>/', update_employee_company, name='update_employee_company'),
