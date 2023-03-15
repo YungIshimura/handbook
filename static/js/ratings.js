6ba8cbca316e34108833363dbbeeceb31c64a071
ghost <kirill.han011@gmail.com>
2023-03-16 00:31:53 +0300

    выполнил рефакторинг кода на Java Script

--- static/js/ratings.js
+++ static/js/ratings.js
@@ -1,8 +1,5 @@
 const ratings = document.querySelectorAll('.rating')
-if (ratings.length > 0) {
     initRatings();
-}
-
 function initRatings() {
     let ratingActive, ratingValue;
     for (let index = 0; index < ratings.length; index++) {
